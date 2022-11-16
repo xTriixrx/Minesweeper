b382685cabfea4109d7acec5776d9097b84e4e6b
xTriixrx <vnigro5743@gmail.com>
2022-11-15 21:20:09 -0500

    Additional board class clean up

--- src/com/qfi/minesweeper/Board.java
+++ src/com/qfi/minesweeper/Board.java
@@ -1,63 +1,43 @@
 package com.qfi.minesweeper;
 
+import java.util.List;
+import java.util.Random;
+import java.util.ArrayList;
+
 public class Board
 {
-	int m_size = 0;
-	private int[][] m_board = null;// = new int[8][8];//main array
-	private int[][] m_selects = null;// = new int[8][8];//array of chosen positions
+	private int m_rowSize = 0;
+	private int m_colSize = 0;
+	private Random m_random = null;
+	private int[][] m_board = null;// = new int[8][8];//main m_boarday
+	private int[][] m_selects = null;// = new int[8][8];//m_boarday of chosen positions
+	private static final int BOMB_FLAG = 9;
+	private static final String SEPARATOR = "--";
 
-	public Board (int size)
+	public Board(int rowSize, int colSize)
 	{
-		m_size = size;
-		m_board = new int[m_size][m_size];
-		m_selects = new int[m_size][m_size];
+		m_rowSize = rowSize;
+		m_colSize = colSize;
+		m_random = new Random();
+		m_board = new int[m_rowSize][m_colSize];
+		m_selects = new int[m_rowSize][m_colSize];
+
+		placeBombs();
+		bombCount();
 	}
 
 	/*
-	 * This method was made to check bomb counts on cells and used to test game
+	 * Places ten "bombs" in both m_boardays randomly sets values for bombs in each m_boarday
 	 */
-	public void PrintBoard(){
-
-		System.out.print("  ");
-
-		for(int i = 0; i < m_size; i++){
-			System.out.print("|" + (i+1) );
-
-		}
-		System.out.print("|");
-		System.out.println();
-		System.out.println("-------------------");
-
-		for(int i = 0; i < m_size; i++){
-			System.out.print((i+m_size + " "));
-			for(int j = 0; j < m_size; j++){
-				if(m_board[i][j] == 0){
-					System.out.print("|-");
-				}
-				else{
-					System.out.print("|" + m_board[i][j]);
-				}
-			}
-			System.out.print("|");
-			System.out.println();
-		}
-
-		System.out.print("-------------------");
-		System.out.println();
-	}
-
-	/*
-	 * Places ten "bombs" in both arrays randomly sets values for bombs in each array
-	 */
-	public void PlaceBombs(){
-		int bomb = 9;
+	private void placeBombs()
+	{
 		int hold, count = 0;
 
 		while(count < 10){
-			for(int i = 0; i < m_board.length; i++){
-				for(int j = 0; j < m_board[0].length; j++){
-					hold = (int)(Math.random() * 25) + 1;
-					if(hold == bomb && count < 10){
+			for(int i = 0; i < m_rowSize; i++){
+				for(int j = 0; j < m_colSize; j++){
+					hold = m_random.nextInt(25) + 1;
+					if(hold == BOMB_FLAG && count < 10){
 						m_board[i][j] = 9;
 						m_selects[i][j] = 11;
 						count++;
@@ -70,9 +50,10 @@ public class Board
 	}
 	
 	/*
-	 * Counts bombs surrounding ith position throughout array 
+	 * Counts bombs surrounding ith position throughout m_boarday
 	 */
-	public void BombCount(){
+	private void bombCount()
+	{
 		int count = 0;
 
 		for(int i = 0; i < m_board.length; i++){
@@ -154,42 +135,17 @@ public class Board
 	}
 
 	/*
-	 * Prints "empty" array board for user
-	 */
-	public void PrintGame(){
-		System.out.print("  ");
-
-		for(int i = 0; i < m_size; i++){
-			System.out.print("|" + (i+1) );
-
-		}
-		System.out.print("|");
-		System.out.println();
-		System.out.println("-------------------");
-
-		for(int i = 0; i < m_size; i++){
-			System.out.print((i+1 + " "));
-			for(int j = 0; j < m_size; j++){
-				if(m_board[i][j] == 0 || m_board[i][j] != 0)
-					System.out.print("|-");
-			}
-			System.out.print("|");
-			System.out.println();
-		}
-
-		System.out.print("-------------------");
-		System.out.println();
-	}
-
-	/*
 	 * Determines what to do when player chooses a specific position
 	 */
-	public void UpdateOnMove(int row, int col){
-		if(m_board[row][col] != 9 && m_board[row][col] != 0){
-			UpdateBoard(row, col);
+	public void submitMove(int row, int col)
+	{
+		if(m_board[row][col] != 9 && m_board[row][col] != 0)
+		{
+			update(row, col);
 		}
 		else{
-			Spread(row, col, m_board);
+			spread(row, col);
+			update(row, col);
 		}
 
 	}
@@ -198,46 +154,217 @@ public class Board
 	 * Updates board to user , outlines all possibilities that could be 
 	 * shown throughout game
 	 */
-	public void UpdateBoard(int row, int col){
+	private void update(int row, int col)
+	{
+		if (m_board[row][col] != 0)
+		{
+			m_selects[row][col] = m_board[row][col];
+		}
+		else
+		{
+			m_selects[row][col] = 10;
+		}
+	}
 
+	/*
+	 * Algorithm for if a spread is called on position, checks 8 surrounding positions to
+	 * one called on
+	 */
+	private void spread(int row, int col)
+	{
+		List<String> spreads = new ArrayList<>();
+
+		if(m_board[row][col] == 0){
+			try{
+				if(m_board[row][col] != 9){
+					if(m_board[row][col] != 0){
+						m_selects[row][col] = m_board[row][col];
+					}
+					if(m_board[row][col] == 0){
+						m_selects[row][col] = 10;
+						spreads.add(row + " " + col);
+					}
+				}
+			}catch(Exception e){
+
+			}
+
+			try{
+				if(m_board[row + 1][col] != 9){
+					if(m_board[row + 1][col] != 0){
+						m_selects[row + 1][col] = m_board[row + 1][col];
+					}
+					if(m_board[row + 1][col] == 0){
+						m_selects[row + 1][col] = 10;
+						spreads.add(row + " " + col);
+					}
+				}
+			}catch(Exception e){
+
+			}
+
+			try{
+				if(m_board[row + 1][col + 1] != 9){
+					if(m_board[row + 1][col + 1] != 0){
+						m_selects[row + 1][col + 1] = m_board[row + 1][col + 1];
+					}
+					if(m_board[row + 1][col + 1] == 0){
+						m_selects[row + 1][col + 1] = 10;
+						spreads.add(row + " " + col);
+					}
+				}
+			}catch(Exception e){
+
+			}
+
+			try{
+				if(m_board[row +1][col - 1] != 9){
+					if(m_board[row +1][col - 1] != 0){
+						m_selects[row + 1][col - 1] = m_board[row +1][col - 1];
+					}
+					if(m_board[row +1][col - 1] == 0){
+						m_selects[row + 1][col - 1] = 10;
+						spreads.add(row + " " + col);
+					}
+				}
+			}catch(Exception e){
+
+			}
+
+			try{
+				if(m_board[row - 1][col] != 9){
+					if(m_board[row - 1][col] != 0){
+						m_selects[row - 1][col] = m_board[row - 1][col];
+					}
+					if(m_board[row - 1][col] == 0){
+						m_selects[row - 1][col] = 10;
+						spreads.add(row + " " + col);
+					}
+				}
+			}catch(Exception e){
+
+			}
+
+			try{
+				if(m_board[row - 1][col + 1] != 9){
+					if(m_board[row - 1][col + 1] != 0){
+						m_selects[row - 1][col + 1] = m_board[row - 1][col + 1];
+					}
+					if(m_board[row - 1][col + 1] == 0){
+						m_selects[row - 1][col + 1] = 10;
+						spreads.add(row + " " + col);
+					}
+				}
+			}catch(Exception e){
+
+			}
+
+			try{
+				if(m_board[row - 1][col - 1] != 9){
+					if(m_board[row - 1][col - 1] != 0){
+						m_selects[row - 1][col - 1] = m_board[row - 1][col - 1];
+					}
+					if(m_board[row - 1][col - 1] == 0){
+						m_selects[row - 1][col - 1] = 10;
+						spreads.add(row + " " + col);
+					}
+				}
+			}catch(Exception e){
+
+			}
+
+			try{
+				if(m_board[row][col - 1] != 9){
+					if(m_board[row][col - 1] != 0){
+						m_selects[row][col - 1] = m_board[row][col - 1];
+					}
+					if(m_board[row][col - 1] == 0){
+						m_selects[row][col - 1] = 10;
+						spreads.add(row + " " + col);
+					}
+				}
+			}catch(Exception e){
+
+			}
+
+			try{
+				if(m_board[row][col + 1] != 9){
+					if(m_board[row][col + 1] != 0){
+						m_selects[row][col + 1] = m_board[row][col + 1];
+					}
+					if(m_board[row][col + 1] == 0){
+						m_selects[row][col + 1] = 10;
+						spreads.add(row + " " + col);
+					}
+				}
+			}catch(Exception e){
+
+			}
+
+		}
+	}
+	
+	/*
+	 * If game is lost, outputs boolean in main class  
+	 */
+	public boolean lostGame(int row, int col)
+	{
+		return m_board[row][col] == 9;
+	}
+	
+	/*
+	 * If is true when only bombs are left, outputs boolean in main class
+	 */
+	public boolean wonGame()
+	{
+		for (int i = 0; i < m_rowSize; i++)
+		{
+			for (int j = 0; j < m_colSize; j++)
+			{
+				if (m_selects[i][j] == 0)
+				{
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	/*
+	 * This method was made to check bomb counts on cells and used to test game
+	 */
+	public void printBoard()
+	{
 		System.out.print("  ");
 
-		for(int i = 0; i < m_size; i++){
+		for(int i = 0; i < m_colSize; i++){
 			System.out.print("|" + (i+1) );
 
 		}
 		System.out.print("|");
 		System.out.println();
-		System.out.println("-------------------");
+		System.out.println(SEPARATOR.repeat(m_colSize + (m_colSize / 4)));
 
-		for(int i = 0; i < m_size; i++){
+		for (int i = 0; i < m_rowSize; i++)
+		{
 			System.out.print((i+1 + " "));
-			for(int j = 0; j < m_size; j++){
-				if(m_selects[i][j] == 10){
+			for (int j = 0; j < m_colSize; j++)
+			{
+				if (m_selects[i][j] == 10)
+				{
 					System.out.print("| ");
 				}
-				else if(m_selects[i][j] == 11){
+				else if (m_selects[i][j] == 11)
+				{
 					System.out.print("|-");
 				}
-				else if(m_selects[i][j] != 0){
+				else if (m_selects[i][j] != 0)
+				{
 					System.out.print("|" + m_selects[i][j]);
 				}
-				else if(m_board[row][col] == 0 && i == row && j == col){
-					System.out.print("| ");
-				}
-				else if(m_board[i][j] != m_board[row][col]){
-					System.out.print("|-");
-				}
-				else if(m_board[i][j] == m_board[row][col]){
-					if(i == row && j == col){
-						System.out.print("|" + m_board[row][col]);
-						m_selects[i][j] = m_board[row][col];
-					}
-					else{
-						System.out.print("|-");
-					}
-				}
-				else{
+				else
+				{
 					System.out.print("|-");
 				}
 			}
@@ -247,189 +374,28 @@ public class Board
 		}
 
 
-		System.out.print("-------------------");
+		System.out.print(SEPARATOR.repeat(m_colSize + (m_colSize / 4)));
 		System.out.println();
-
-		/* was used to test selects array inputs
-		for(int i = 0; i < selects.length; i++){
-			for(int j = 0; j < selects[0].length; j++){
-				System.out.print(selects[i][j]);
-			}
-			System.out.println();
-		}
-		 */
-	}
-
-	/*
-	 * Algorithm for if a spread is called on position, checks 8 surrounding positions to
-	 * one called on
-	 */
-	public void Spread(int row, int col, int[][] arr){
-		if(arr[row][col] == 0){
-			try{
-				if(arr[row][col] != 9){
-					if(arr[row][col] != 0){
-						m_selects[row][col] = arr[row][col];
-					}
-					if(arr[row][col] == 0){
-						m_selects[row][col] = 10;
-					}
-				}
-			}catch(Exception e){
-
-			}
-
-			try{
-				if(arr[row + 1][col] != 9){
-					if(arr[row + 1][col] != 0){
-						m_selects[row + 1][col] = arr[row + 1][col];
-					}
-					if(arr[row + 1][col] == 0){
-						m_selects[row + 1][col] = 10;
-					}
-				}
-			}catch(Exception e){
-
-			}
-
-			try{
-				if(arr[row + 1][col + 1] != 9){
-					if(arr[row + 1][col + 1] != 0){
-						m_selects[row + 1][col + 1] = arr[row + 1][col + 1];
-					}
-					if(arr[row + 1][col + 1] == 0){
-						m_selects[row + 1][col + 1] = 10;
-					}
-				}
-			}catch(Exception e){
-
-			}
-
-			try{
-				if(arr[row +1][col - 1] != 9){
-					if(arr[row +1][col - 1] != 0){
-						m_selects[row + 1][col - 1] = arr[row +1][col - 1];
-					}
-					if(arr[row +1][col - 1] == 0){
-						m_selects[row + 1][col - 1] = 10;
-					}
-				}
-			}catch(Exception e){
-
-			}
-
-			try{
-				if(arr[row - 1][col] != 9){
-					if(arr[row - 1][col] != 0){
-						m_selects[row - 1][col] = arr[row - 1][col];
-					}
-					if(arr[row - 1][col] == 0){
-						m_selects[row - 1][col] = 10;
-					}
-				}
-			}catch(Exception e){
-
-			}
-
-			try{
-				if(arr[row - 1][col + 1] != 9){
-					if(arr[row - 1][col + 1] != 0){
-						m_selects[row - 1][col + 1] = arr[row - 1][col + 1];
-					}
-					if(arr[row - 1][col + 1] == 0){
-						m_selects[row - 1][col + 1] = 10;
-					}
-				}
-			}catch(Exception e){
-
-			}
-
-			try{
-				if(arr[row - 1][col - 1] != 9){
-					if(arr[row - 1][col - 1] != 0){
-						m_selects[row - 1][col - 1] = arr[row - 1][col - 1];
-					}
-					if(arr[row - 1][col - 1] == 0){
-						m_selects[row - 1][col - 1] = 10;
-					}
-				}
-			}catch(Exception e){
-
-			}
-
-			try{
-				if(arr[row][col - 1] != 9){
-					if(arr[row][col - 1] != 0){
-						m_selects[row][col - 1] = arr[row][col - 1];
-					}
-					if(arr[row][col - 1] == 0){
-						m_selects[row][col - 1] = 10;
-					}
-				}
-			}catch(Exception e){
-
-			}
-
-			try{
-				if(arr[row][col + 1] != 9){
-					if(arr[row][col + 1] != 0){
-						m_selects[row][col + 1] = arr[row][col + 1];
-					}
-					if(arr[row][col + 1] == 0){
-						m_selects[row][col + 1] = 10;
-					}
-				}
-			}catch(Exception e){
-
-			}
-
-		}
-		UpdateBoard(row, col);
 	}
 	
 	/*
-	 * If game is lost, outputs boolean in main class  
-	 */
-	public boolean LoseGame(int row, int col){
-		if(m_board[row][col] == 9){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	
-	/*
-	 * If is true when only bombs are left, outputs boolean in main class
-	 */
-	public boolean WinGame(int row, int col){
-		for(int i = 0; i < m_size; i++){
-			for(int j = 0; j < m_size; j++){
-				if(m_selects[i][j] == 0){
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-	/*
-	 * Is called at the end of game (if game is lost) displays bombs as Qs and all selected 
+	 * Is called at the end of game (if game is lost) displays bombs as Qs and all selected
 	 * positions
 	 */
-	public void EndGameBoard(){
+	public void printEndBoard(){
 		System.out.print("  ");
 
-		for(int i = 0; i < m_size; i++){
+		for(int i = 0; i < m_colSize; i++){
 			System.out.print("|" + (i+1) );
 
 		}
 		System.out.print("|");
 		System.out.println();
-		System.out.println("-------------------");
+		System.out.println(SEPARATOR.repeat(m_colSize + (m_colSize / 4)));
 
-		for(int i = 0; i < m_size; i++){
+		for(int i = 0; i < m_rowSize; i++){
 			System.out.print((i+1 + " "));
-			for(int j = 0; j < m_size; j++){
+			for(int j = 0; j < m_colSize; j++){
 				if(m_board[i][j] == 9){
 					System.out.print("|Q");
 				}
@@ -450,7 +416,7 @@ public class Board
 			System.out.println();
 		}
 
-		System.out.print("-------------------");
+		System.out.print(SEPARATOR.repeat(m_colSize + (m_colSize / 4)));
 		System.out.println();
 	}
 
--- src/com/qfi/minesweeper/Main.java
+++ src/com/qfi/minesweeper/Main.java
@@ -14,85 +14,82 @@ public class Main
 
 		Scanner scan = new Scanner(System.in);
 
-		Board b = new Board(maxSize);
-		
-		b.PrintGame();
+		Board b = new Board(maxSize, maxSize);
+		b.printBoard();
 
-		b.PlaceBombs();
-
-		b.BombCount();
-		
-		//b.PrintBoard();
-
-		
-		do{
-		System.out.print("Input row: ");
-		while(rowCheck){
-			try{
-				row = scan.nextInt();
-				row = row - 1;
-				if(row <= -1 || row > (maxSize - 1)){
-					rowCheck = true;
+		do
+		{
+			System.out.print("Input row: ");
+			while(rowCheck){
+				try{
+					row = scan.nextInt();
+					row = row - 1;
+					if(row <= -1 || row > (maxSize - 1)){
+						rowCheck = true;
+						System.out.print("This value is not in the range of the game try again");
+						System.out.println();
+					}
+					else{
+					rowCheck = false;
+					}
+				}catch(Exception e){
 					System.out.print("This value is not in the range of the game try again");
-					System.out.println();
 				}
-				else{
-				rowCheck = false;
-				}
-			}catch(Exception e){
-				System.out.print("This value is not in the range of the game try again");
 			}
-		}
 
-		System.out.print("Input column: ");
-		while(colCheck){
-			try{
-				col = scan.nextInt();
-				col = col - 1;
-				if(col <= -1 || col > (maxSize - 1)){
-					colCheck = true;
+			System.out.print("Input column: ");
+			while(colCheck)
+			{
+				try
+				{
+					col = scan.nextInt();
+					col = col - 1;
+					if(col <= -1 || col > (maxSize - 1)){
+						colCheck = true;
+						System.out.print("This value is not in the range of the game try again");
+						System.out.println();
+					}
+					else{
+					colCheck = false;
+					}
+				}catch(Exception e){
 					System.out.print("This value is not in the range of the game try again");
-					System.out.println();
 				}
-				else{
-				colCheck = false;
-				}
-			}catch(Exception e){
-				System.out.print("This value is not in the range of the game try again");
 			}
-		}
-		if (firsttime){
-		StartTime = System.nanoTime();
-		firsttime = false;
-		}
-		
-		b.UpdateOnMove(row, col);
-		
-		rowCheck = true;
-		colCheck = true;
-		
-		if(b.LoseGame(row, col)){
-			DecimalFormat df = new DecimalFormat("###.##");
-			EndTime = System.nanoTime();
-			Gameplay = false;
-			double seconds = (EndTime-StartTime) / 1000000000.0;
-			int minutes = (int) (seconds / 60);
-			seconds = seconds - (minutes * 60);
-			b.EndGameBoard();
-			System.out.println("Your time was: " + minutes + " Minute(s), " +  df.format(seconds) + " second(s).");
-			System.out.println("Sorry, you lost");
-		}
-		if(b.WinGame(row, col)){
-			DecimalFormat df = new DecimalFormat("###.##");
-			EndTime = System.nanoTime();
-			Gameplay = false;
-			double seconds = (EndTime-StartTime) / 1000000000.0;
-			int minutes = (int) (seconds / 60);
-			seconds = seconds - (minutes * 60);
-			System.out.println("Congratulations, you won!");
-			System.out.println("Your time was: " + minutes + " Minute(s), " +  df.format(seconds) + " second(s).");
-		}
-		
+			if (firsttime){
+			StartTime = System.nanoTime();
+			firsttime = false;
+			}
+
+			b.submitMove(row, col);
+			b.printBoard();
+
+			rowCheck = true;
+			colCheck = true;
+
+			if (b.lostGame(row, col))
+			{
+				DecimalFormat df = new DecimalFormat("###.##");
+				EndTime = System.nanoTime();
+				Gameplay = false;
+				double seconds = (EndTime-StartTime) / 1000000000.0;
+				int minutes = (int) (seconds / 60);
+				seconds = seconds - (minutes * 60);
+				b.printEndBoard();
+				System.out.println("Your time was: " + minutes + " Minute(s), " +  df.format(seconds) + " second(s).");
+				System.out.println("Sorry, you lost");
+			}
+			if (b.wonGame())
+			{
+				DecimalFormat df = new DecimalFormat("###.##");
+				EndTime = System.nanoTime();
+				Gameplay = false;
+				double seconds = (EndTime-StartTime) / 1000000000.0;
+				int minutes = (int) (seconds / 60);
+				seconds = seconds - (minutes * 60);
+				System.out.println("Congratulations, you won!");
+				System.out.println("Your time was: " + minutes + " Minute(s), " +  df.format(seconds) + " second(s).");
+			}
 		}while(Gameplay);
 	}
 
